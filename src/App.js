f5208aeb9e267194a390b645d4fd374221588317
kallyl <kallyl_lacerda@hotmail.com>
2019-04-13 02:23:22 -0300

    Aplication frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import Routes from './routes'
+
+const App = () => <Routes />;
+
+// function App () {
+//   return <Main />;
+// }
+
+export default App;
